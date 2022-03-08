a48a8cd73cac1d14970449bdb60651ab3a5cd69b
sastofficial <39431922+sastofficial@users.noreply.github.com>
2022-03-08 20:53:53 +0100

    fix later :)

---
+++ gdtools.py
@@ -0,0 +1,15 @@
+# GD TOOLS 1.0 by SA ST
+loop = 0
+from os import path
+if (path.exists("options.gdtools")):
+    optionfile = open("options.gdtools", "w")
+
+
+
+
+
+# remember list
+# https://www.w3schools.com/python/python_file_write.asp
+# https://www.w3schools.com/python/python_file_open.asp
+# https://www.guru99.com/python-check-if-file-exists.html
+# <3
