806df4c4b1d2774f76e09b1437fc7bfe3c90cec7
sastofficial <39431922+sastofficial@users.noreply.github.com>
2022-03-09 17:08:11 +0100

    save for later

--- gdtools.py
+++ gdtools.py
@@ -1,15 +1,28 @@
 # GD TOOLS 1.0 by SA ST
 loop = 0
 from os import path
-if (path.exists("options.gdtools")):
+from urllib import request
+
+
+option_exist = path.exists("options.gdtools")
+if option_exist == False:
+    print("Optionfile not found. Creating a new one.")
     optionfile = open("options.gdtools", "w")
-
-
-
-
-
-# remember list
-# https://www.w3schools.com/python/python_file_write.asp
-# https://www.w3schools.com/python/python_file_open.asp
-# https://www.guru99.com/python-check-if-file-exists.html
-# <3
+    optionfile.write("False")
+else:
+    optionfile = open("options.gdtools", "w")
+    print("Optionfile found.")
+if optionfile.readline == "False":
+    requestloop = 0
+    while requestloop < 1:
+        requests = input("Do you have requests installed (Y/N)\n")
+        if requests == "n" or "N":
+            print('Please install requests using "pip install requests"\n')
+            exit()
+        elif requests == "y" or "Y":
+            print("Saving Optionfile")
+            optionfile.write("True")
+            requestloop = requestloop + 1
+        else:
+            print("Error! Please try again.")
+print("test")
