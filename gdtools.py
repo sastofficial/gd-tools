5389749dce42365723684aaaf77c2d0706e1fa40
sastofficial <39431922+sastofficial@users.noreply.github.com>
2022-03-09 17:31:44 +0100

    IT WORKSSSS

--- gdtools.py
+++ gdtools.py
@@ -16,12 +16,12 @@ readoption = open("options.gdtools", "r")
 request_option = readoption.readline() 
 if request_option == "False":
     requestloop = 0
-    while requestloop < 1:
+    while requestloop == 0:
         requests = input("Do you have requests installed (y/n) (CASE SENSETIVE)\n")
         if requests == "n":
             print('Please install requests using "pip install requests"\n')
             exit()
-        elif requests == "y" or "Y":
+        elif requests == "y":
             print("Saving Optionfile")
             optionwrite = open("options.gdtools", "w")
             optionwrite.write("True")
