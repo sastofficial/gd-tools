611d34e3368c5afb7c06e05eb637be3ae87449d4
sastofficial <39431922+sastofficial@users.noreply.github.com>
2022-03-09 18:16:28 +0100

    I GOT DOWNLOADING WORKING :)))))))))))))))))

--- gdtools.py
+++ gdtools.py
@@ -30,5 +30,28 @@ while nooptionloop == 0:
                 requestloop = 1
             else:
                 print("Error! Please try again.")
-
-
+print("""  ____ ____  _____ ___   ___  _     ____
+ / ___|  _ \|_   _/ _ \ / _ \| |   / ___|
+| |  _| | |   | || | | | | | | |   \___ )
+| |_| | |_|   | || |_| | |_| | |___ ___) |
+ \____|____/  |_| \___/ \___/|_____|____/
+ BY SA ST
+""")
+from requests import get
+while loop == 0:
+    command = input("> ")
+    if command == "exit":
+        print("Thanks for using GD TOOLS\n")
+        exit()
+    elif command == "download -s":
+        song_download_id = input("Enter LevelID: ")
+        song_download_gd = get('https://gdbrowser.com/api/level/' + song_download_id)
+        song_download_songlink = song_download_gd.json()['songLink']
+        song_download_mp3 = get(song_download_songlink, stream = True)
+        with open(song_download_gd.json()['songName'] + ".mp3", "wb") as song_download_file:
+            for song_download_chunk in song_download_mp3.iter_content(chunk_size = 16*1024):
+                song_download_file.write(song_download_chunk) # thank you https://365datascience.com/tutorials/python-tutorials/python-requests-package/
+                print("Song downloaded. Look for the .mp3 file")
+    elif command == help:
+    else:
+        print("Error")
